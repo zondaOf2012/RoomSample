1ec3bfb5621399b3c1f2cacc2f6353f872dd9d27
zonda <zhuqiuzhu@foxmail.com>
2018-06-22 16:38:07 +0800

    study-migrate:定义 schema 路径，生成schema支持数据库迁移，及测试

--- app/src/main/kotlin/room/arch/zonda/roomwordsample/persistence/WordRoomDatabase.kt
+++ app/src/main/kotlin/room/arch/zonda/roomwordsample/persistence/WordRoomDatabase.kt
@@ -4,10 +4,11 @@ import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.migration.Migration
 import android.content.Context
 import kotlinx.coroutines.experimental.async
 
-@Database(entities = [Word::class] , version = 1, exportSchema = false)
+@Database(entities = [Word::class] , version = 1, exportSchema = true)
 abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
@@ -24,6 +25,12 @@ abstract class WordRoomDatabase : RoomDatabase() {
                     }
                 }
 
+        private val MIGRATION_1_2 = object : Migration(1, 2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE users ADD COLUMN last_update INTEGER")
+            }
+        }
+
         private fun buildDatabase(context: Context) =
                 Room
                         //配置构建数据库基本参数
@@ -31,6 +38,7 @@ abstract class WordRoomDatabase : RoomDatabase() {
                                 context.applicationContext,
                                 WordRoomDatabase::class.java,
                                 "WordSample.db")
+                        //数据库迁移设置为丢弃之前版本的数据
                         .fallbackToDestructiveMigration()
                         //预加载数据库
                         .addCallback(object : RoomDatabase.Callback() {
@@ -52,6 +60,7 @@ abstract class WordRoomDatabase : RoomDatabase() {
                                 wordDao.insert(word)
                             }
                         })
+                        .addMigrations(MIGRATION_1_2)
                         //正式创建数据库
                         .build()
     }
